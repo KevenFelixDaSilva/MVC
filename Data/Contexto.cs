b6c3eb579c170645f22d3580a946fa6bd437cf63
KevenFelixDaSilva <114019476+KevenFelixDaSilva@users.noreply.github.com>
2023-01-24 20:36:31 -0300

    inserçao de projetos

---
+++ Data/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dev_Junior
+{
+	public class Contexto : DbContext
+	{
+		
+	}
+    
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dev_Junior.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Display(Name="Código")]
+        [Column("Id")]
+        public int Id {get; set;}
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        public string Nome {get; set;}
+    }
+}
