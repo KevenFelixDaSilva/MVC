f98717be8c3481b092a22f5959e2a6652127d359
KevenFelixDaSilva <114019476+KevenFelixDaSilva@users.noreply.github.com>
2023-01-25 13:34:25 -0300

    inserçao da conn do SqlServe

---
+++ Data/ConecxaoSql.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace dev_Junior.Data
+{
+    public class ConecxaoSql
+    {
+        public void conecxaoSql(string[] args)
+        {
+            string connectionString = $"Data Source=DESKTOP-M3OQ381;Initial Catalog=keven;Integrated Security=True;";
+         
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+
+                    cmd.CommandText = "SELECT * FROM Contacts";
+
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+
+                    while (dr.Read())
+                    {
+                        string firstName = dr["FirstName"].ToString();
+                        string lastName = dr["LastName"].ToString();
+
+                        Console.WriteLine(firstName + " " + lastName);
+                    }
+
+                    dr.Close();
+                }
+            }
+        }
+    }
+}
+           
