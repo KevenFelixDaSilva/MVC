9e179c3e9f6a59dc66f5e0e8be7d83f841e0c049
KevenFelixDaSilva <114019476+KevenFelixDaSilva@users.noreply.github.com>
2023-01-25 02:58:40 -0300

    implementaçao cnn

---
+++ Data/SqlClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dev_Junior
+{
+    public class SqlClient
+    {
+       
+       
+        private static string strConn { get; set; }
+        private string Conn;
+
+        SqlConnection conn = new SqlConnection(strConn); 
+    
+    }
+    public string ConnectionString { get; set; }  
+    using (SqlConnection connection = new SqlConnection(connectionString))  
+    {  
+
+        connection.Open();   
+    }      
+}
+    
+
